8853178adbdf27dd181aeceb65e7454261cb6117
AbialiDoctor <luckybelt79@gmail.com>
2024-04-25 19:36:48 +0530

    second page added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import Image from "next/image";
-import Hero from "../../components/dashboard/landingpage/hero/Hero";
+import Image from 'next/image';
+import Hero from '../../components/dashboard/landingpage/hero/Hero';
+import WelcomePG from '../../components/dashboard/landingpage/WelcomePG';
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <WelcomePG />
     </>
   );
 }
